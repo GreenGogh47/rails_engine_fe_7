cd2ce475fbafd004bbc296deffe4867a833bbed4
Chris Crane <chrisjcrane@gmail.com>
2023-04-05 16:33:07 -0600

    create route, controller, and view

--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -3,8 +3,13 @@ require "rails_helper"
 RSpec.describe "Merchant Index Page" do
   describe "As a visitor" do
     describe "when I visit '/merchants'" do
-      xit "shows a list of merchants by name" do
+      it "shows a list of merchants by name" do
+        visit "/merchants"
 
+        expect(page).to have_content("Merchants")
+        expect(page).to have_content("Schroeder-Jerde")
+        expect(page).to have_content("Klein, Rempel and Jones")
+        expect(page).to have_content("Willms and Sons")
       end
 
       xit "their name links to their show page" do
