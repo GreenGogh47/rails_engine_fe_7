10c85adf7d25f2ea07dc280a511d4fad38623950
Chris Crane <chrisjcrane@gmail.com>
2023-04-05 15:16:32 -0600

    add tests for user story merchants

---
+++ spec/features/merchant/index_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Index Page" do
+  describe "As a visitor" do
+    describe "when I visit '/merchants'" do
+      xit "shows a list of merchants by name" do
+
+      end
+
+      xit "their name links to their show page" do
+
+      end
+    end
+  end
+end
---
+++ spec/features/merchant/show_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Show Page" do
+  describe "As a visitor" do
+    describe "when I visit '/merchants/:id'" do
+      xit "shows a list of items that merchant sells" do
+
+      end
+    end
+  end
+end
