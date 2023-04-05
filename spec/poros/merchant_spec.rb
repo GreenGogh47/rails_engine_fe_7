a0bb2a62227f293c98568c573ef6f72f7858e25e
Chris Crane <chrisjcrane@gmail.com>
2023-04-05 16:20:18 -0600

    add tests for poro

---
+++ spec/poros/merchant_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe Merchant do
+  before(:each) do
+    @jerde = {
+      "id": "1",
+      "type": "merchant",
+      "attributes": {
+          "name": "Schroeder-Jerde"
+      }
+    }
+
+    @merchant = Merchant.new(@jerde)
+  end
+
+  it "exists" do
+    expecct(@merchant).to be_a(Merchant)
+    expect(@merchant.id).to eq(1)
+    expect(@merchant.type).to eq("merchant")
+    expect(@merchant.name).to eq("Schroeder-Jerde")
+  end
+end
