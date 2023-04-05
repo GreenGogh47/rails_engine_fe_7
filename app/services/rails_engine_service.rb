d8fc452ec2ee2e98a674776fbe792c359f60ac8b
Chris Crane <chrisjcrane@gmail.com>
2023-04-05 15:50:20 -0600

    pass first api consumption test

---
+++ app/services/rails_engine_service.rb
@@ -0,0 +1,14 @@
+class RailsEngineService
+  def merchants
+    get_url("merchants")
+  end
+  
+  def get_url(url)
+    response = connection.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def connection
+    Faraday.new(url: "http://localhost:3000/api/v1/")
+  end
+end
