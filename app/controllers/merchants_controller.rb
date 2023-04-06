8a69e146e31936471fa841052d2839da6b9ff24d
Chris Crane <chrisjcrane@gmail.com>
2023-04-06 11:27:00 -0600

    start view

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -4,7 +4,9 @@ class MerchantsController < ApplicationController
   end
   
   def show
+    require 'pry'; binding.pry
     @merchant = facade.merchants.find { |merchant| merchant.id == params[:id] }
+    @items = facade.items
   end
 
   def facade
--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "Merchant Show Page" do
         expect(page).to have_content("Schroeder-Jerde")
       end
 
-      xit "shows a list of items that merchant sells" do
+      it "shows a list of items that merchant sells" do
         expect(page).to have_content("Item Nemo Facere")
         expect(page).to have_content("Item Expedita Aliquam")
       end
