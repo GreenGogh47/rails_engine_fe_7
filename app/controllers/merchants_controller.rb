e88806ba9e12e0b4ed4b00c81e92872a72f42eb6
Chris Crane <chrisjcrane@gmail.com>
2023-04-06 13:10:56 -0600

    add items to merchant show page

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -4,10 +4,11 @@ class MerchantsController < ApplicationController
   end
   
   def show
-    require 'pry'; binding.pry
     @merchant = facade.merchants.find { |merchant| merchant.id == params[:id] }
-    @items = facade.items
+    @items = facade.items(params[:id])
   end
+
+  private
 
   def facade
     @facade ||= RailsEngineFacade.new
